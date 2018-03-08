23161b083265b868133f2195dc65bd67bd975261
Jiri Prajzner <jprajzne@redhat.com>
2018-03-08 21:33:38 +0100

    UPD: creating 8 cards

--- matchez.js
+++ matchez.js
@@ -1,23 +1,31 @@
-var clicks = 0;
+function createGameGrid() {
+  var img = null;
+  for(var number = 0; number < 8; number++) {
+    img = document.createElement('img');
+    img.className = 'card';
+    img.id = number;
+    img.src = 'graphics/guitarists/top.jpg';
+    img.setAttribute('onclick', 'turnOver(event)');
+    document.body.appendChild(img);
+  }
+}
 
+var clicks = 0;
 function turnOver(event) {
   clicks++;
   //console.log('clicks: '+ clicks);
   if(clicks > 2) {
     return false;
   }
-  //console.log(event.target || event.srcElement);
+  //console.log('event source'+ event.target || event.srcElement);
   var eventSourceImg = event.target || event.srcElement;
   if(eventSourceImg.id === '1') {
-    eventSourceImg.src = "graphics/guitarists/0001-jimi.hendrix.jpg";
-  } else if (eventSourceImg.id === '2') {
-    eventSourceImg.src = "graphics/guitarists/0002-duane.allman.jpg";
-  } else if (eventSourceImg.id === '3') {
     eventSourceImg.src = "graphics/guitarists/003-bb.king.jpg";
   }
+
   var stres = setTimeout(function() {
     eventSourceImg.src="graphics/guitarists/top.jpg";
     clicks = 0;}, 3000);
   //console.log('settimeout result: '+ stres);
+  
 }
-
